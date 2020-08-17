7d4198deee255c4c6aa8466551b6cfbe78d99046
eync <eync@users.noreply.github.com>
2020-08-17 12:37:44 +0300

    huge upgrade

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 let homePage= `
 
-<blockquote>
+<blockquote style="line-height:3vh;">
   <header>making difference</header>
   <p>Empathy, visualization, anticipation of behavior and communication are Sanjiv's underlying strengths.</p>
 </blockquote>
+<div style="display:block;font-family: var(--lf);line-height:1vh;color: var(--jacky);text-align:center;font-size:120px;padding:15px;margin:0;">-</div>
+<p style="font-size:18px;line-height: 2em;text-align:center;">
+I am 26 years old from Helsinki Finland, I am open-minded, cheerful, very social and I understand bigger entities. I love to learn something new and hold on to it!
+</p>
 
-
+<a href="#" onclick="onNavItemClick('/about'); return false;" class="btn" style="">more about <span style="font-family: var(--lf);"> Sanjiv ➥</span></div>
 `;
